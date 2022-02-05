31b97cb090939cd469bc1b9f2959d77c5829dd52
arunabha212 <arunabha.dutt@mca.christuniversity.in>
2022-02-05 14:00:18 +0530

    CAT 2

---
+++ CAT 2/val.js
@@ -0,0 +1,256 @@
+function validateform() {
+    console.log('Inside fucn');
+    if (checkname1()) {
+        if (checkaddress()) {
+            if (checkemail()) {
+                if (checkbranch()) {
+                    if (checkcollege()) {
+                        if (check_age) {
+                            if (checkphn) {
+                                if (checkpassword)
+                                    return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+
+}
+function check_name(event) {
+    let code = event.which;
+    if (code > 47 && code < 58)
+        return false
+    else
+        return true;
+}
+function checkname1() {
+    if (!checkname()) {
+        document.getElementById("address").disabled = true;
+        document.getElementById("Email").disabled = true;
+        document.getElementById("branch").disabled = true;
+        document.getElementById("college").disabled = true;
+        document.getElementById("Age").disabled = true;
+        document.getElementById("phonenumber").disabled = true;
+        document.getElementById("uname").disabled = true;
+        document.getElementById("password").disabled = true;
+        document.getElementById("repassword").disabled = true;
+        return false;
+    }
+    if (checkname()) {
+        document.getElementById("address").disabled = false;
+        document.getElementById("Email").disabled = false;
+        document.getElementById("branch").disabled = false;
+        document.getElementById("college").disabled = false;
+        document.getElementById("Age").disabled = false;
+        document.getElementById("phonenumber").disabled = false;
+        document.getElementById("uname").disabled = false;
+        document.getElementById("password").disabled = false;
+        document.getElementById("repassword").disabled = false;
+        return true;
+    }
+}
+function checkname() {
+    var name = document.getElementById('name').value;
+    if (name == "") {
+        alert('Please enter a name!');
+        document.getElementById('name').style.borderColor = "red";
+        document.getElementById('name').style.borderWidth = "thick";
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+function checkemail() {
+    var email = document.getElementById('Email').value;
+    const regex1 = /^([a-z0-9\.-]+)@university.in$/;
+    //console.log(email);
+    if (regex1.test(email)) {
+        return true;
+
+    }
+    else {
+        alert('Email is not valid!')
+        document.getElementById('Email').style.borderColor = "red";
+        document.getElementById('Email').style.borderWidth = "thick";
+        return false;
+    }
+}
+function checksame() {
+    var password = document.getElementById('password').value;
+    var repassword = document.getElementById('repassword').value;
+    if (password != repassword) {
+        alert('Passwords dont match!')
+        return false;
+    }
+    else
+        return true;
+}
+function checkaddress() {
+    if (document.getElementById('address').value == "") {
+        alert('Address cannot be blank!')
+        document.getElementById('address').style.borderColor = "red";
+        document.getElementById('address').style.borderWidth = "thick";
+        return false;
+    }
+    else
+        document.getElementById('address').style.borderColor = "green";
+        return true;
+}
+function check() {
+    var password = document.getElementById('password').value;
+    var flag = 1;
+    console.log(document.getElementById('password').value);
+    if (password == "") {
+        document.getElementById('password').style.borderColor = "red";
+        document.getElementById('password').style.borderWidth = "thick";
+        document.getElementById('message').textContent = "Password cannot be blank";
+        document.getElementById('message').style.color = "red";
+        flag = 0;
+        return false;
+    }
+    else if (password.length < 5) {
+        document.getElementById('password').style.borderColor = "red";
+        document.getElementById('password').style.borderWidth = "thick";
+        document.getElementById('message').textContent = "Password is too small ";
+        document.getElementById('message').style.color = "red";
+        flag = 0;
+        return false;
+    }
+    re = /[0-9]/;
+    if (!re.test(password)) {
+        document.getElementById('password').style.borderColor = "red";
+        document.getElementById('password').style.borderWidth = "thick";
+        document.getElementById('message').textContent = "Number is missing";
+        document.getElementById('message').style.color = "red";
+        flag = 0;
+        return false;
+    }
+    re = /[a-z]/;
+    if (!re.test(password)) {
+        document.getElementById('password').style.borderColor = "red";
+        document.getElementById('password').style.borderWidth = "thick";
+        document.getElementById('message').textContent = "Small letters are missing";
+        document.getElementById('message').style.color = "red";
+        flag = 0;
+        return false;
+    }
+    re = /[A-Z]/;
+    if (!re.test(password)) {
+        document.getElementById('password').style.borderColor = "red";
+        document.getElementById('password').style.borderWidth = "thick";
+        document.getElementById('message').textContent = "Password must contain at least one uppercase letter (A-Z)!";
+        document.getElementById('message').style.color = "red";
+        flag = 0;
+        return false;
+    }
+    re = /[ ~`!@#$%^&*()-_+={}[\]\?\;:"<>,.]/
+    if (!re.test(password)) {
+        document.getElementById('password').style.borderColor = "red";
+        document.getElementById('password').style.borderWidth = "thick";
+        document.getElementById('message').textContent = "Password must contain at least one special character!";
+        document.getElementById('message').style.color = "red";
+        flag = 0;
+        return false;
+    }
+    re = /([A-Za-z0-9~`!@#$%^&*()-_+={}[\]|;:"<>,./\\?])+/
+    if (!re.test(password)) {
+        document.getElementById('password').style.borderColor = "red";
+        document.getElementById('password').style.borderWidth = "thick";
+        document.getElementById('message').textContent = " Password must contain at least one special character in the middle!";
+        document.getElementById('message').style.color = "red";
+        flag = 0;
+        return false;
+    }
+    if (flag) {
+        document.getElementById('message').textContent = "";
+        document.getElementById('password').style.borderColor = "green";
+        document.getElementById('password').style.borderWidth = "thick";
+        return true;
+    }
+}
+
+function checkphn() {
+    var phn = document.getElementById('phonenumber').value;
+    const regex1 = /^\d{10}$/;
+    if (regex1.test(phn)) {
+        return true;
+
+    }
+    else {
+        alert('Phone is not valid!')
+        document.getElementById('phonenumber').style.borderColor = "red";
+        document.getElementById('phonenumber').style.borderWidth = "thick";
+        return false;
+    }
+}
+function check_age() {
+    var age = document.getElementById('Age').value;
+    if (age < 0) {
+        alert('Age cannot be negative!')
+        document.getElementById('age').style.borderColor = "red";
+        document.getElementById('age').style.borderWidth = "thick";
+        return false;
+    }
+    else
+        return true;
+}
+
+function checkuname() {
+    if (document.getElementById('uname').value == "") {
+        alert('Username cannot be blank!')
+        document.getElementById('uname').style.borderColor = "red";
+        document.getElementById('uname').style.borderWidth = "thick";
+        return false;
+    }
+    else
+        return true;
+}
+function setCookie() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("Email").value;
+    alert('Cookies accepted!!!')
+    document.cookie = "username=" + name + ";expires=Fri,9 Feb 2022 04:00:00 UTC;"
+    document.cookie = "emailId=" + email + ";expires=Fri,10 Feb 2022 01:00:00 UTC;";
+}
+window.onload = function () {
+    if (document.cookie.length != 0) {
+        var color, i, img;
+        var arr = document.cookie.split(";");
+        console.log(arr)
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i].includes("username=")) {
+                username = arr[i].split("=")[1];
+            }
+            if (arr[i].includes("emailId")) {
+                email = arr[i].split("=")[1];
+            }
+        }
+        document.getElementById("name").value = username;
+        document.getElementById("email").value = email;
+    }
+}
+
+function checkbranch() {
+    if (document.getElementById('branch').value == "") {
+        alert('Branch cannot be blank!')
+        document.getElementById('branch').style.borderColor = "red";
+        document.getElementById('branch').style.borderWidth = "thick";
+        return false;
+    }
+    else
+        return true;
+}
+function checkcollege() {
+    if (document.getElementById('college').value == "") {
+        alert('College cannot be blank!')
+        document.getElementById('college').style.borderColor = "red";
+        document.getElementById('college').style.borderWidth = "thick";
+        return false;
+    }
+    else
+        return true;
+}
