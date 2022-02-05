5d5a38b69ee24dfdb6d284659a13bbf7239dc443
arunabha212 <arunabha.dutt@mca.christuniversity.in>
2022-02-05 08:49:36 +0530

    angular js updated

--- angular.js
+++ angular.js
@@ -5,6 +5,40 @@ angular.module("bill", [])
             return input.substring(0, 1).toUpperCase() + input.substring(1);
         }
     })
+    .filter("showdate",function(){
+        return function(input)
+        {
+            var arr= input.split("-")
+            var month
+            //console.log(arr)
+            if(arr[1]=="01")
+            month="Jan"
+            else if (arr[1]=="02")
+            month="Feb"
+            else if (arr[1]=="03")
+            month="Mar"
+            else if (arr[1]=="04")
+            month="Apr"
+            else if (arr[1]=="05")
+            month="May"
+            else if (arr[1]=="06")
+            month="Jun"
+            else if (arr[1]=="07")
+            month="Jul"
+            else if (arr[1]=="08")
+            month="Aug"
+            else if (arr[1]=="09")
+            month="Sep"
+            else if (arr[1]=="10")
+            month="Oct"
+            else if (arr[1]=="11")
+            month="Nov"
+            else
+            month="Dec"
+
+            return arr[2]+'-'+month+'-'+arr[0];
+        }
+    })
     .controller("billctrl", function ($scope, $http) {
         $http.get('https://arunabha212.github.io/JSON/bill.json')
             .success(function (response) {
