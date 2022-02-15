ddada9f5bd64dcd1440075fe96c4020f4371da77
arunabha212 <arunabha.dutt@mca.christuniversity.in>
2022-02-16 03:06:30 +0530

    node-update

--- node/nodejson.js
+++ node/nodejson.js
@@ -1,3 +1,5 @@
 const {readFileSync}=require('fs')
 var loadjson=()=>JSON.parse(readFileSync('bill.json'))
+var data=readFileSync('bill.json')
+console.log(JSON.parse(data)['billitems'][0])
 module.exports={loadjson};
--- node/nodeserver.js
+++ node/nodeserver.js
@@ -1,14 +1,36 @@
-const http=require('http')
-const {loadjson}=require('./nodejson')
-const express =require('express')
-const app=express();
-const port=7000;
-const cors=require('cors')
+const http = require('http')
+const { loadjson } = require('./nodejson')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express();
+const port = 7000;
+const cors = require('cors');
 app.use(cors({
-    orgin:"*",
+    orgin: "*",
 }))
-app.listen(port,()=> console.log("Server is running...!"))
-app.get('/getjson', (req,res)=>
-{
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => console.log("Server is running...!"))
+app.get('/getjson', (req, res) => {
     res.send(loadjson())
+})
+app.get('/add', (req, res) => {
+    res.sendFile(__dirname + '/jsonadd.html');
+})
+app.post('/submit', (req, res) => {
+    console.log(JSON.stringify(req.body))
+    const fs = require("fs")
+    var data = fs.readFileSync("bill.json");
+    
+    var myObj = JSON.parse(data);
+    console.log(myObj)
+    var item =req.body
+    myObj['billitems'].push(item);
+    var newData2 = JSON.stringify(myObj);
+    fs.writeFile("bill.json", newData2, (err) => {
+
+        if (err) throw err;
+        console.log("New data added");
+    });
+    res.send('Data updated!!');
 })
